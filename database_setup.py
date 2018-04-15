3f6a7f604cc384a992481375a5bf17c948bd5ba0
Erwin Bredy <ebredy@gmail.com>
2018-04-14 22:40:37 -0400

    made change to your database setup.
    I set column to 1000 from 250

--- database_setup.py
+++ database_setup.py
@@ -14,7 +14,7 @@ class Destinations(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(1000), nullable=False)
 
     @property
     def serialize(self):
